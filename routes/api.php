0beab47ae75d722c4441a5600ab50925d3d2618f
Anver Dole <mohomadanver@gmail.com>
2020-11-14 18:56:51 +0530

    push app first time

--- routes/api.php
+++ routes/api.php
@@ -15,24 +15,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-// Route::get('/login', 'App\\Http\\Controllers\\LoginController@Login')->middleware('guest');
-// Route::get('/register', 'App\\Http\\Controllers\\RegisterController@Register')->middleware('guest:api');
-// Route::get('/logout', 'App\\Http\\Controllers\\LoginController@Logout')->middleware('auth:api');
-// Route::get("/subjects" ,'App\\Http\\Controllers\\SubjectController@index')->middleware('auth:api');
+Route::get('/login', 'App\\Http\\Controllers\\LoginController@Login')->middleware('guest');
+Route::get('/register', 'App\\Http\\Controllers\\RegisterController@Register')->middleware('guest:api');
+Route::get('/logout', 'App\\Http\\Controllers\\LoginController@Logout')->middleware('auth:api');
+Route::get("/subjects" ,'App\\Http\\Controllers\\SubjectController@index')->middleware('auth:api');
 
-// Route::get("/instructor/exams" ,'App\\Http\\Controllers\\InstructorExamController@index')->middleware(['auth:api' ,'authrole:instructor']);
-// Route::get("/instructor/exams/new-exam" ,'App\\Http\\Controllers\\InstructorExamController@new')->middleware(['auth:api' ,'authrole:instructor']);
-// Route::get("/instructor/exams/edit-exam" ,'App\\Http\\Controllers\\InstructorExamController@edit')->middleware(['auth:api' ,'authrole:instructor']);
-// Route::get("/instructor/exams/exam" ,'App\\Http\\Controllers\\InstructorExamController@getexam')->middleware(['auth:api' ,'authrole:instructor']);
-// Route::get("/instructor/exam/delete" ,'App\\Http\\Controllers\\InstructorExamController@deleteexam')->middleware(['auth:api' ,'authrole:instructor']);
+Route::get("/instructor/exams" ,'App\\Http\\Controllers\\InstructorExamController@index')->middleware(['auth:api' ,'authrole:instructor']);
+Route::get("/instructor/exams/new-exam" ,'App\\Http\\Controllers\\InstructorExamController@new')->middleware(['auth:api' ,'authrole:instructor']);
+Route::get("/instructor/exams/edit-exam" ,'App\\Http\\Controllers\\InstructorExamController@edit')->middleware(['auth:api' ,'authrole:instructor']);
+Route::get("/instructor/exams/exam" ,'App\\Http\\Controllers\\InstructorExamController@getexam')->middleware(['auth:api' ,'authrole:instructor']);
+Route::get("/instructor/exam/delete" ,'App\\Http\\Controllers\\InstructorExamController@deleteexam')->middleware(['auth:api' ,'authrole:instructor']);
 
 
-// Route::get("/student/exams" ,'App\\Http\\Controllers\\StudentExamController@index')->middleware(['auth:api' ,'authrole:student']);
-// Route::get("/student/exams/finished" ,'App\\Http\\Controllers\\StudentExamController@finished')->middleware(['auth:api' ,'authrole:student']);
-// Route::get("/student/exams/exam/info" ,'App\\Http\\Controllers\\StudentExamController@examinfo')->middleware(['auth:api' ,'authrole:student']);
+Route::get("/student/exams" ,'App\\Http\\Controllers\\StudentExamController@index')->middleware(['auth:api' ,'authrole:student']);
+Route::get("/student/exams/finished" ,'App\\Http\\Controllers\\StudentExamController@finished')->middleware(['auth:api' ,'authrole:student']);
+Route::get("/student/exams/exam/info" ,'App\\Http\\Controllers\\StudentExamController@examinfo')->middleware(['auth:api' ,'authrole:student']);
 
-// Route::get("/student/exams/exam/take" ,'App\\Http\\Controllers\\ExamController@take')->middleware(['auth:api' ,'authrole:student']);
-// Route::get("/student/exams/exam/take/save" ,'App\\Http\\Controllers\\ExamController@save')->middleware(['auth:api' ,'authrole:student']);
-// Route::get("/student/exams/exam/take/info" ,'App\\Http\\Controllers\\ExamController@info')->middleware(['auth:api' ,'authrole:student']);
-// Route::get("/student/exams/exam/take/submit" ,'App\\Http\\Controllers\\ExamController@submit')->middleware(['auth:api' ,'authrole:student']);
-// Route::get("/student/exams/exam/take/summery" ,'App\\Http\\Controllers\\ExamController@summery')->middleware(['auth:api' ,'authrole:student']);
+Route::get("/student/exams/exam/take" ,'App\\Http\\Controllers\\ExamController@take')->middleware(['auth:api' ,'authrole:student']);
+Route::get("/student/exams/exam/take/save" ,'App\\Http\\Controllers\\ExamController@save')->middleware(['auth:api' ,'authrole:student']);
+Route::get("/student/exams/exam/take/info" ,'App\\Http\\Controllers\\ExamController@info')->middleware(['auth:api' ,'authrole:student']);
+Route::get("/student/exams/exam/take/submit" ,'App\\Http\\Controllers\\ExamController@submit')->middleware(['auth:api' ,'authrole:student']);
+Route::get("/student/exams/exam/take/summery" ,'App\\Http\\Controllers\\ExamController@summery')->middleware(['auth:api' ,'authrole:student']);
--- routes/web.php
+++ routes/web.php
@@ -14,31 +14,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
-
-Route::get('/login', 'App\\Http\\Controllers\\LoginController@Login');
-Route::get('/register', 'App\\Http\\Controllers\\RegisterController@Register')->middleware('guest');
-Route::get('/logout', 'App\\Http\\Controllers\\LoginController@Logout')->middleware('auth');
-Route::get("/subjects" ,'App\\Http\\Controllers\\SubjectController@index')->middleware('auth');
-
-Route::get("/instructor/exams" ,'App\\Http\\Controllers\\InstructorExamController@index')->middleware(['auth' ,'authrole:instructor']);
-Route::get("/instructor/exams/new-exam" ,'App\\Http\\Controllers\\InstructorExamController@new')->middleware(['auth' ,'authrole:instructor']);
-Route::get("/instructor/exams/edit-exam" ,'App\\Http\\Controllers\\InstructorExamController@edit')->middleware(['auth' ,'authrole:instructor']);
-Route::get("/instructor/exams/exam" ,'App\\Http\\Controllers\\InstructorExamController@getexam')->middleware(['auth' ,'authrole:instructor']);
-Route::get("/instructor/exam/delete" ,'App\\Http\\Controllers\\InstructorExamController@deleteexam')->middleware(['auth' ,'authrole:instructor']);
-
-
-Route::get("/student/exams" ,'App\\Http\\Controllers\\StudentExamController@index')->middleware(['auth' ,'authrole:student']);
-Route::get("/student/exams/finished" ,'App\\Http\\Controllers\\StudentExamController@finished')->middleware(['auth' ,'authrole:student']);
-Route::get("/student/exams/exam/info" ,'App\\Http\\Controllers\\StudentExamController@examinfo')->middleware(['auth' ,'authrole:student']);
-
-Route::get("/student/exams/exam/take" ,'App\\Http\\Controllers\\ExamController@take')->middleware(['auth' ,'authrole:student']);
-Route::get("/student/exams/exam/take/save" ,'App\\Http\\Controllers\\ExamController@save')->middleware(['auth' ,'authrole:student']);
-Route::get("/student/exams/exam/take/info" ,'App\\Http\\Controllers\\ExamController@info')->middleware(['auth' ,'authrole:student']);
-Route::get("/student/exams/exam/take/submit" ,'App\\Http\\Controllers\\ExamController@submit')->middleware(['auth' ,'authrole:student']);
-Route::get("/student/exams/exam/take/summery" ,'App\\Http\\Controllers\\ExamController@summery')->middleware(['auth' ,'authrole:student']);
-
-Route::get("/" , function(){
-    dd(User::all());
-});
