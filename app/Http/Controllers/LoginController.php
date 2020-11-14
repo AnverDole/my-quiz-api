d09b1087e6b08eb28f4e57289935bdbf84155e48
Anver Dole <mohomadanver@gmail.com>
2020-11-14 18:23:48 +0530

    push app first time

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Hash;
 class LoginController extends Controller
 {
     public function Login(Request $request){
-        dd($request->all());
+        
         $data = (object)$request->validate([
             'email' => 'email|required|exists:users,email',
             'password' => 'required',
@@ -28,6 +28,7 @@ class LoginController extends Controller
                 'email' => $user->email,
             ]);
         }else{
+            dd($request->all());
            throw ValidationException::withMessages([
                'email' => 'You have entered an invalid username or password'
            ]);
